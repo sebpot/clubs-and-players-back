684f13a0d00acced2277bf1c1a02a8182f4a7893
Sebastian Potrykus <seba05092001@gmail.com>
2022-11-28 20:59:43 +0100

    Lab3 - adding gateway v2

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,32 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route("club", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/clubs", "/api/clubs/**")
+						.uri("http://localhost:8081"))
+				.route("player", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/players", "/api/players/**")
+						.uri("http://localhost:8082"))
+				.build();
+	}
+}

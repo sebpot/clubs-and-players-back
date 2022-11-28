6a1229d0b54a453b5d6ac98e46c345f36e9d3fb0
Sebastian Potrykus <seba05092001@gmail.com>
2022-11-28 20:58:40 +0100

    Lab3 - adding gateway

--- lab3_player/src/main/java/com/example/demo/DemoApplication.java
+++ lab3_player/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	@Qualifier("club")
+	public RestTemplate restTemplate(){
+		return new RestTemplateBuilder()
+				.rootUri("http://localhost:8080/api/clubs")
+				.build();
+	}
 }
